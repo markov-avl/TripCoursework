cb99f13fc173cd271f772aadb27b3c656dd87332
CreatorOfEverything <annoyingfox159@gmail.com>
2023-03-18 20:01:21 +1000

    Добавлена страница с ОД

--- controller/index_controller.py
+++ controller/index_controller.py
@@ -1,10 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 from controller.method import Method
+from service import CityService
 
 blueprint = Blueprint('index', __name__)
+city_service = CityService()
 
 
 @blueprint.route('/', methods=[Method.GET])
 def index():
-    return 'Hello!'
+    cities = sorted(city_service.get_all(), key=lambda c: c.name)
+
+    return render_template(
+        'data_editor.jinja2',
+        cities=cities
+    )
+
+# @blueprint.route('/editor', methods=[Method.GET])
+# def _index():
+#     cities = sorted(city_service.get_all(), key=lambda c: c.name)
+#
+#     return render_template(
+#         'data_editor.jinja2',
+#         cities=cities
+#     )
